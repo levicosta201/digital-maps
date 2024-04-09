df76c9d951c8166c1b05967c1f07f37fce82dac7
levi <levi.costa1@gmail.com>
2024-04-09 13:32:20 -0300

    Remove default view from Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Api Digital Maps',
+        'version' => '0.1'
+    ]);
 });
